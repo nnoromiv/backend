9db1713a7c6b7f20bb6cdb62c21aa922eb439d6a
nnoromiv <nnorom.prince44@gmail.com>
2025-10-18 22:18:48 +0100

    AWS Connection

--- db/connection.py
+++ db/connection.py
@@ -10,8 +10,9 @@ Base = declarative_base()
 
 engine = create_engine(
     f"postgresql+psycopg2://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}"
-    f"@/cloudsql/{os.getenv('POSTGRES_HOST')}/{os.getenv('POSTGRES_DB')}"
+    f"@{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_DB')}"
 )
+
 # Session factory
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
